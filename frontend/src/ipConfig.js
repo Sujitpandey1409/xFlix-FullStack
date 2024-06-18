f4a747adc010380cb8e84a379e0772cb107858aa
Sujitpandey1409 <sujitpandey1409@gmail.com>
2024-06-18 21:29:24 +0530

    setup for deployment

--- frontend/src/ipConfig.js
+++ frontend/src/ipConfig.js
@@ -1,3 +1,4 @@
 //let mockServer = {"endpoint": "https://xflix-backend-bicn.onrender.com"};
-let mockServer = {"endpoint": "http://localhost:8082"};
+// let mockServer = {"endpoint": "http://localhost:8082"};
+let mockServer = {"endpoint": "https://xflix-fullstack-8pil.onrender.com"};
 export default mockServer;
